d0275d09bcfcbf7c2637046c9312cf118667afad
Ricardo <rmgss11@iscte-iul.pt>
2019-11-20 23:45:52 +0000

    Long_metho feito, já é gerado o valor booleano, mas naõ é guardado em nenhum ficheiro

--- Bad Code/src/GUI.java
+++ Bad Code/src/GUI.java
@@ -12,9 +12,11 @@ import java.io.IOException;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -36,8 +38,8 @@ public class GUI {
 	private JCheckBox envy_button;
 	private JButton start_button;
 	private JTable table;
-	
-	
+
+
 	public GUI() {
 		frame = new JFrame("GUI");
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -51,19 +53,23 @@ public class GUI {
 	public void open() {
 		frame.setVisible(true);
 	}
-	
-	
+
+
 	private void addFrameContent() {
 		//////////////////envy and long buttons
 		envy_button = new JCheckBox();
 		JLabel feature_envy_label = new JLabel("Feature Envy");
-		JTextField envy_threshold = new JTextField("");
-		envy_threshold.setPreferredSize(new Dimension(100, 20));
-		envy_threshold.setEditable(false);
+		JTextField envy_ATFD = new JTextField("ATFD");
+		JTextField envy_LAA = new JTextField("LAA");		
+		envy_ATFD.setPreferredSize(new Dimension(100, 20));
+		envy_LAA.setPreferredSize(new Dimension(100, 20));
+		envy_ATFD.setEditable(false);
+		envy_LAA.setEditable(false);
 		envy_button.addItemListener(new ItemListener(){
 			@Override
 			public void itemStateChanged(ItemEvent arg0) {
-				envy_threshold.setEditable((envy_button).isSelected());
+				envy_ATFD.setEditable((envy_button).isSelected());
+				envy_LAA.setEditable((envy_button).isSelected());
 				check_logic_button();
 			}
 		});
@@ -72,31 +78,35 @@ public class GUI {
 		logic_function_threshold = new JComboBox(new Object[] {"AND", "OR"});
 		logic_function_label.setVisible(false);
 		logic_function_threshold.setVisible(false);
-		
-		
+
+
 		long_button = new JCheckBox();
 		JLabel long_method_label = new JLabel("Long Method");
-		JTextField long_threshold = new JTextField("");
-		long_threshold.setPreferredSize(new Dimension(100, 20));
-		long_threshold.setEditable(false);
+		JTextField long_LOC = new JTextField("Min");
+		JTextField long_CYCLO = new JTextField("Max");
+		long_LOC.setPreferredSize(new Dimension(100, 20));
+		long_CYCLO.setPreferredSize(new Dimension(100, 20));
+		long_LOC.setEditable(false);
+		long_CYCLO.setEditable(false);
 		long_button.addItemListener(new ItemListener(){
 			@Override
 			public void itemStateChanged(ItemEvent arg0) {
-				long_threshold.setEditable((long_button).isSelected());
+				long_LOC.setEditable((long_button).isSelected());
+				long_CYCLO.setEditable((long_button).isSelected());
 				check_logic_button();
 			}
 		});
 		///////////////////////////////////////////////////
-		
-		
+
+
 
 		JButton search_file = new JButton("Search File");
 		JTextField search_file_result = new JTextField("");
 		search_file_result.setPreferredSize(new Dimension(100, 20));
 		search_file_result.setEditable(false);
-		
-		
-		
+
+
+
 		search_file.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -108,94 +118,111 @@ public class GUI {
 					search_file_result.setText(excelPath);
 				}
 			}
-			
+
 		});
 
-		
-		
+
 		start_button = new JButton("Start");
 		start_button.addActionListener(new ActionListener(){
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				// TODO Auto-generated method stub
-				String feature_envy_treshold = envy_threshold.getText();
-				String long_method_treshold	=long_threshold.getText();
+				//fazer confirmação de int
+				//ler ficheiro
 				String logic = (String) logic_function_threshold.getSelectedItem();
-				System.out.println("feature_envy_treshold " + feature_envy_treshold);
-				System.out.println("logic function "+ logic);
-				System.out.println("long_method_treshold " + long_method_treshold);
-				
+				if(long_button.isSelected()) {
+					try {
+						int locThreshold=Integer.parseInt(long_LOC.getText());
+						int cycloThreshold=Integer.parseInt(long_CYCLO.getText());
+						for(int row = 0; row<table.getRowCount(); row++) {
+							int locFunction=(int)Double.parseDouble(table.getModel().getValueAt(row, 4).toString());
+							int cycloFunction=(int)Double.parseDouble(table.getModel().getValueAt(row, 5).toString());
+							boolean isLongMethod = (locFunction> locThreshold && cycloFunction>cycloThreshold);
+							//o resultado para cado funcao esta no isLongMethod
+							//table.setValueAt(isLongMethod, row, 8);
+						}
+					}catch(NumberFormatException e) {
+						JOptionPane.showMessageDialog(null, "Introduza numeros inteiros");
+					}
+				}
+				if(envy_button.isSelected()) {
+					int atfd=Integer.parseInt(envy_ATFD.getText());
+					int laa=Integer.parseInt(envy_LAA.getText());
+				}
 			}
-			
+
 		});
-		
+
+
 		//ScrollPane com tabela
 		table = new JTable();
 		JScrollPane jsp = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-		
+
 		//botão import
 		JButton import_excel = new JButton("Import Excel");
 		import_excel.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-					if(search_file_result.getText() != "") {
-						XSSFWorkbook workbook = importExcel(search_file_result.getText());
-						if(workbook != null) {
-							XSSFSheet excelSheet = workbook.getSheetAt(0);
-							String[] headerrow= {"methodID", "package", "class", "method", "loc", 
-									"cyclo", "atfd", "laa", "is_long_method", "iplasma", "pmd", "is_feature_envy"};
-							DefaultTableModel dtm = new DefaultTableModel(null, headerrow);
-							for(int row = 1; row < excelSheet.getLastRowNum() + 1; row++) {
-								XSSFRow excelRow = excelSheet.getRow(row);
-								
-								XSSFCell methodID = excelRow.getCell(0);
-								XSSFCell pacote = excelRow.getCell(1);
-								XSSFCell classe = excelRow.getCell(2);
-								XSSFCell method = excelRow.getCell(3);
-								XSSFCell loc = excelRow.getCell(4);
-								XSSFCell cyclo = excelRow.getCell(5);
-								XSSFCell atfd = excelRow.getCell(6);
-								XSSFCell laa = excelRow.getCell(7);
-								XSSFCell is_long_method = excelRow.getCell(8);
-								XSSFCell iplasma = excelRow.getCell(9);
-								XSSFCell pmd = excelRow.getCell(10);
-								XSSFCell is_feature_envy = excelRow.getCell(11);
-								
-								dtm.addRow(new Object[] { methodID, pacote, classe, method, loc, cyclo, atfd, laa,
-										is_long_method, iplasma, pmd, is_feature_envy});
-								
-							}
-							table.setModel(dtm);
+				if(search_file_result.getText() != "") {
+					XSSFWorkbook workbook = importExcel(search_file_result.getText());
+					if(workbook != null) {
+						XSSFSheet excelSheet = workbook.getSheetAt(0);
+						String[] headerrow= {"methodID", "package", "class", "method", "loc", 
+								"cyclo", "atfd", "laa", "is_long_method", "iplasma", "pmd", "is_feature_envy"};
+						DefaultTableModel dtm = new DefaultTableModel(null, headerrow);
+						for(int row = 1; row < excelSheet.getLastRowNum() + 1; row++) {
+							XSSFRow excelRow = excelSheet.getRow(row);
+
+							XSSFCell methodID = excelRow.getCell(0);
+							XSSFCell pacote = excelRow.getCell(1);
+							XSSFCell classe = excelRow.getCell(2);
+							XSSFCell method = excelRow.getCell(3);
+							XSSFCell loc = excelRow.getCell(4);
+							XSSFCell cyclo = excelRow.getCell(5);
+							XSSFCell atfd = excelRow.getCell(6);
+							XSSFCell laa = excelRow.getCell(7);
+							XSSFCell is_long_method = excelRow.getCell(8);
+							XSSFCell iplasma = excelRow.getCell(9);
+							XSSFCell pmd = excelRow.getCell(10);
+							XSSFCell is_feature_envy = excelRow.getCell(11);
+
+							dtm.addRow(new Object[] { methodID, pacote, classe, method, loc, cyclo, atfd, laa,
+									is_long_method, iplasma, pmd, is_feature_envy});
+
 						}
+						table.setModel(dtm);
 					}
-						
+				}
+
 			}
 		});
-		
-		
+
+
 		/////////////
 		JPanel feature_envy = new JPanel();
 		feature_envy.add(envy_button);
 		feature_envy.add(feature_envy_label);
-		feature_envy.add(envy_threshold);
+		feature_envy.add(envy_ATFD);
+		feature_envy.add(envy_LAA);
 
 		JPanel logic_function = new JPanel();
 		logic_function.add(logic_function_label);
 		logic_function.add(logic_function_threshold);
-		
-		
+
+
 		JPanel long_method = new JPanel();
 		long_method.add(long_button);
 		long_method.add(long_method_label);
-		long_method.add(long_threshold);
+		long_method.add(long_LOC);
+		long_method.add(long_CYCLO);
 
 		JPanel left = new JPanel(new GridLayout(3,1));
 		left.add(feature_envy);
 		left.add(logic_function);
 		left.add(long_method);
-		
+
 		JPanel right = new JPanel();
 		right.add(start_button);
 
@@ -203,7 +230,7 @@ public class GUI {
 		search.add(search_file);
 		search.add(search_file_result);
 		search.add(import_excel);
-		
+
 
 		JPanel upper = new JPanel(new BorderLayout());
 		upper.add(left, BorderLayout.WEST);
@@ -223,7 +250,7 @@ public class GUI {
 		frame.add(panel);
 		//////////////
 	}
-	
+
 	public void check_logic_button() {
 		Boolean visible = long_button.isSelected() && envy_button.isSelected();
 		logic_function_label.setVisible(visible);
@@ -241,7 +268,7 @@ public class GUI {
 		}
 		return workbook;
 	}
-	
+
 	public static void main(String[] args) {
 		GUI window = new GUI();
 		window.open();
