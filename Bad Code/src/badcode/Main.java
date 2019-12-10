7d66d5521dc441134f7afc5be7c22278f0eed2dd
Ricardo <rmgss11@iscte-iul.pt>
2019-12-10 23:07:37 +0000

    Optimização do código

--- Bad Code/src/badcode/Main.java
+++ Bad Code/src/badcode/Main.java
@@ -1,8 +1,11 @@
 package badcode;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 import javax.swing.JOptionPane;
 
@@ -16,7 +19,11 @@ public class Main {
 	private GUI gui;
 	private ArrayList<ExcelMethod> list;
 
-	Main() {
+	private enum FaultType {
+		DCI, DII, ADCI, ADII;
+	}
+
+	public Main() {
 		this.gui = new GUI(this);
 		this.list = new ArrayList<>();
 		open();
@@ -56,17 +63,32 @@ public class Main {
 		return workbook;
 	}
 
-	public void analyzeTable(int locThreshold, int cycloThreshold, String lmLogic, int aftdThreshold, double laaThreshold, String feLogic) {
+	private FaultType getFaultType(boolean a, boolean b) {
+		FaultType temp = null;
+		if (a && b) {
+			temp = FaultType.DCI;
+		} else if (!a && b) {
+			temp = FaultType.DII;
+		} else if (a && !b) {
+			temp = FaultType.ADII;
+		} else if (!a && !b) {
+			temp = FaultType.ADCI;
+		}
+		return temp;
+	}
+
+	public void analyzeTable(int locThreshold, int cycloThreshold, String lmLogic, int aftdThreshold,
+			double laaThreshold, String feLogic) {
 		if (list.size() != 0) {
 			boolean isLongMethod = false;
 			boolean isFeatureEnvy = false;
-			
+
 			int nMethods = 0;
 
 			int dciPlasma = 0, diiPlasma = 0;
 			int adiiPlasma = 0, adciPlasma = 0;
 			String indPlasma = "";
-			
+
 			int dciPMD = 0, diiPMD = 0;
 			int adiiPMD = 0, adciPMD = 0;
 			String indPMD = "";
@@ -77,149 +99,116 @@ public class Main {
 
 			int dciEnvy = 0, diiEnvy = 0;
 			int adciEnvy = 0, adiiEnvy = 0;
-			String indUFE = "-";			
-			
-			//criar janela de resultados
-			
-			String[] header = {"MethodID", "iPlasma", "PMD", "UserLongMethod", "UserFeatureEnvy"};
+			String indUFE = "-";
+
+			ArrayList<FaultType> iPlasmasFaults = new ArrayList<FaultType>();
+			ArrayList<FaultType> PMDFaults = new ArrayList<FaultType>();
+			ArrayList<FaultType> userLMFaults = new ArrayList<FaultType>();
+			ArrayList<FaultType> userFEFaults = new ArrayList<FaultType>();
+
+			// criar janela de resultados
+
+			String[] header = { "MethodID", "iPlasma", "PMD", "UserLongMethod", "UserFeatureEnvy" };
 			Results resultado = new Results(header);
-	
+
 			for (int row = 0; row < list.size(); row++) {
 				ExcelMethod currentMethod = list.get(row);
 				nMethods++;
 				int locFunction = currentMethod.getLoc();
 				int cycloFunction = currentMethod.getCyclo();
-				if(lmLogic == "and")
-					isLongMethod = (locFunction > locThreshold && cycloFunction > cycloThreshold);
-				else if(lmLogic=="or")
-					isLongMethod = (locFunction > locThreshold || cycloFunction > cycloThreshold);
-				// o resultado para cada função está no isLongMethod
-
+				int atfdExcelValue = currentMethod.getAtfd();
+				double laaExcelValue = currentMethod.getLaa();
 				// ler booleans da tabela
+				boolean isFeatureEnvyExcelValue = currentMethod.isFeatureEnvy();
 				boolean isLongMethodExcelValue = currentMethod.isLongMethod();
 				boolean iPlasmaExcelValue = currentMethod.getiPlasma();
 				boolean pmdExcelValue = currentMethod.getPmd();
 
-				// comparação iplasma
-				if (isLongMethodExcelValue && iPlasmaExcelValue) {
-					dciPlasma++;
-					indPlasma = "DCI";
-				} else if (!isLongMethodExcelValue && iPlasmaExcelValue) {
-					diiPlasma++;
-					indPlasma = "DII";
-				} else if (isLongMethodExcelValue && !iPlasmaExcelValue) {
-					adiiPlasma++;
-					indPlasma = "ADII";
-				} else if(!isLongMethodExcelValue && !iPlasmaExcelValue){
-					adciPlasma++;
-					indPlasma = "ADCI";
-				}
+				// o resultado para cada função está no isLongMethod
+				if (lmLogic == "and")
+					isLongMethod = (locFunction > locThreshold && cycloFunction > cycloThreshold);
+				else if (lmLogic == "or")
+					isLongMethod = (locFunction > locThreshold || cycloFunction > cycloThreshold);
 
-				// comparação pmd
-				if (isLongMethodExcelValue && pmdExcelValue) {
-					dciPMD++;
-					indPMD = "DCI";
-				} else if (!isLongMethodExcelValue && pmdExcelValue) {
-					diiPMD++;
-					indPMD= "DII";
-				} else if (isLongMethodExcelValue && !pmdExcelValue) {
-					adiiPMD++;
-					indPMD= "ADII";
-				} else if(!isLongMethodExcelValue && !pmdExcelValue){
-					adciPMD++;
-					indPMD = "ADCI";
-				}
-
-				
-				// comparação regras user long
-				if(!lmLogic.equals("")) {
-					if (isLongMethodExcelValue && isLongMethod) {
-						dciUser++;
-						indULM = "DCI";
-					} else if (!isLongMethodExcelValue && isLongMethod) {
-						diiUser++;
-						indULM = "DII";
-					} else if (isLongMethodExcelValue && !isLongMethod) {
-						adiiUser++;
-						indULM = "ADII";
-					} else if(!isLongMethodExcelValue && !isLongMethod){
-						adciUser++;
-						indULM = "ADCI";
-					}
-				}
-				int atfdExcelValue = currentMethod.getAtfd();
-				double laaExcelValue = currentMethod.getLaa();
-				if(feLogic == "and")
-				isFeatureEnvy = (atfdExcelValue > aftdThreshold && laaExcelValue < laaThreshold);
-				if(feLogic == "or")
-					isFeatureEnvy = (atfdExcelValue > aftdThreshold || laaExcelValue < laaThreshold);
 				// o resultado para cada função está no isEnvyFeature
+				if (feLogic == "and")
+					isFeatureEnvy = (atfdExcelValue > aftdThreshold && laaExcelValue < laaThreshold);
+				if (feLogic == "or")
+					isFeatureEnvy = (atfdExcelValue > aftdThreshold || laaExcelValue < laaThreshold);
 
-				boolean isFeatureEnvyExcelValue = currentMethod.isFeatureEnvy();
-
-				// comparação regras user envy
-				if(!feLogic.equals("")) {
-					if (isFeatureEnvyExcelValue && isFeatureEnvy) {
-						dciEnvy++;
-						indUFE = "DCI";
-					} else if (!isFeatureEnvyExcelValue && isFeatureEnvy) {
-						diiEnvy++;
-						indUFE = "DII";
-					} else if (isFeatureEnvyExcelValue && !isFeatureEnvy) {
-						adiiEnvy++;
-						indUFE = "ADII";
-					} else if(!isFeatureEnvyExcelValue && !isFeatureEnvy){
-						adciEnvy++;
-						indUFE = "ADCI";
-					}
+				// comparação iplasma
+				FaultType aux = getFaultType(isLongMethodExcelValue, iPlasmaExcelValue);
+				indPlasma = aux.toString();
+				iPlasmasFaults.add(aux);
+				// comparação pmd
+				aux = getFaultType(isLongMethodExcelValue, pmdExcelValue);
+				indPMD = aux.toString();
+				PMDFaults.add(aux);
+				// comparação regras user long
+				if (!lmLogic.equals("")) {
+					aux = getFaultType(isLongMethodExcelValue, isLongMethod);
+					indULM = aux.toString();
+					userLMFaults.add(aux);
 				}
-				String[] newRow = {String.valueOf(currentMethod.getMethodID()), indPlasma, indPMD, indULM, indUFE};
+				// comparação regras user envy
+				if (!feLogic.equals("")) {
+					aux = getFaultType(isFeatureEnvyExcelValue, isFeatureEnvy);
+					indUFE =  aux.toString();
+					userFEFaults.add(aux);
+				}
+				String[] newRow = { String.valueOf(currentMethod.getMethodID()), indPlasma,
+						indPMD, indULM, indUFE };
 				resultado.addRow(newRow);
 			}
 
-			// resultado experimental
-			/*
-			System.out.println("NUMBER OF METHODS:    " + nMethods);
-			System.out.println("DCI iPLASMA:   " + dciPlasma + ";  DII iPLASMA:   " + diiPlasma);
-			System.out.println("ADCI iPLASMA:   " + adciPlasma + ";  ADII iPLASMA:   " + adiiPlasma);
-			System.out.println("DCI PMD:       " + dciPMD + ";  DII PMD:       " + diiPMD);
-			System.out.println("ADCI PMD:       " + adciPMD + ";  ADII PMD:       " + adiiPMD);
-			System.out.println("DCI USER LONG: " + dciUser + ";  DII USER LONG: " + diiUser);
-			System.out.println("ADCI USER LONG: " + adciUser + ";  ADII USER LONG: " + adiiUser);
-			System.out.println("DCI USER ENVY: " + dciEnvy + ";  DII USER ENVY: " + diiEnvy);
-			System.out.println("ADCI USER ENVY: " + adciEnvy + ";  ADII USER ENVY: " + adiiEnvy);
-			 */
-			
-			//array com todos os valores necessários para apresentar ao user
-			int[][] data = {{nMethods},
-					{dciPlasma, diiPlasma, adiiPlasma, adciPlasma},
-					{dciPMD, diiPMD, adiiPMD, adciPMD},
-					{dciUser, diiUser, adiiUser, adciUser},
-					{dciEnvy, diiEnvy, adiiEnvy, adciEnvy}};
-			String[] tiposDefeitos = {"DCI","DII","ADII","ADCI"};
+			dciPlasma = Collections.frequency(iPlasmasFaults, FaultType.DCI);
+			diiPlasma = Collections.frequency(iPlasmasFaults, FaultType.DII);
+			adiiPlasma = Collections.frequency(iPlasmasFaults, FaultType.ADII);
+			adciPlasma = Collections.frequency(iPlasmasFaults, FaultType.ADCI);
+
+			dciPMD = Collections.frequency(PMDFaults, FaultType.DCI);
+			diiPMD = Collections.frequency(PMDFaults, FaultType.DII);
+			adiiPMD = Collections.frequency(PMDFaults, FaultType.ADII);
+			adciPMD = Collections.frequency(PMDFaults, FaultType.ADCI);
+
+			dciUser = Collections.frequency(userLMFaults, FaultType.DCI);
+			diiUser = Collections.frequency(userLMFaults, FaultType.DII);
+			adiiUser = Collections.frequency(userLMFaults, FaultType.ADII);
+			adciUser = Collections.frequency(userLMFaults, FaultType.ADCI);
+
+			dciEnvy = Collections.frequency(userFEFaults, FaultType.DCI);
+			diiEnvy = Collections.frequency(userFEFaults, FaultType.DII);
+			adiiEnvy = Collections.frequency(userFEFaults, FaultType.ADII);
+			adciEnvy = Collections.frequency(userFEFaults, FaultType.ADCI);
+
+			// array com todos os valores necessários para apresentar ao user
+			int[][] data = { { nMethods }, { dciPlasma, diiPlasma, adiiPlasma, adciPlasma },
+					{ dciPMD, diiPMD, adiiPMD, adciPMD }, { dciUser, diiUser, adiiUser, adciUser },
+					{ dciEnvy, diiEnvy, adiiEnvy, adciEnvy } };
+			String[] tiposDefeitos = { "DCI", "DII", "ADII", "ADCI" };
 			String[] tiposInfo = tiposInfoPedido(lmLogic, feLogic);
-			
+
 			resultado.addResults(data, tiposDefeitos, tiposInfo);
-			
+
 			resultado.displayResults();
 		} else {
 			JOptionPane.showMessageDialog(null, "Importe um Ficheiro Excel");
 		}
 	}
-	
-	//Tipos de informação dependendo das boxes assinaladas
+
+	// Tipos de informação dependendo das boxes assinaladas
 	private String[] tiposInfoPedido(String lm, String fe) {
-		if(lm.equals("") && fe.equals("")) {
-			String[] ini = {"iPlasma", "PMD"};
+		if (lm.equals("") && fe.equals("")) {
+			String[] ini = { "iPlasma", "PMD" };
 			return ini;
-		} else if(lm.equals("") && !fe.equals("")) {
-			String[] ini = {"iPlasma", "PMD", "UserFeatureEnvy"};
+		} else if (lm.equals("") && !fe.equals("")) {
+			String[] ini = { "iPlasma", "PMD", "UserFeatureEnvy" };
 			return ini;
-		} else if(!lm.equals("") && fe.equals("")) {
-			String[] ini = {"iPlasma", "PMD", "UserLongMethod"};
+		} else if (!lm.equals("") && fe.equals("")) {
+			String[] ini = { "iPlasma", "PMD", "UserLongMethod" };
 			return ini;
 		} else {
-			String[] ini = {"iPlasma", "PMD", "UserLongMethod", "UserFeatureEnvy"};
+			String[] ini = { "iPlasma", "PMD", "UserLongMethod", "UserFeatureEnvy" };
 			return ini;
 		}
 	}
