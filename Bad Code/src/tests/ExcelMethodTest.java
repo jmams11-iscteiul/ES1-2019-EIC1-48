eebc0f11986c52073b455caba337473bbe4e57ed
jmcrm-iscteiul <jmcrm@iscte-iul.pt>
2019-12-10 23:16:56 +0000

    File fix

--- Bad Code/src/tests/ExcelMethodTest.java
+++ Bad Code/src/tests/ExcelMethodTest.java
@@ -56,7 +56,7 @@ class ExcelMethodTest {
 		workbook = new XSSFWorkbook(new FileInputStream(excelPath));
 		XSSFSheet excelSheet = workbook.getSheetAt(0);
 		XSSFRow excelRow = excelSheet.getRow(1);
-		
+
 		excelMethod = new ExcelMethod(excelRow);
 
 		methodID = Integer.parseInt(excelRow.getCell(0).getRawValue());
@@ -73,151 +73,145 @@ class ExcelMethodTest {
 		isFeatureEnvy = excelRow.getCell(11).getBooleanCellValue();
 	}
 
-/**
- * @throws java.lang.Exception
- */
-@AfterAll
-static void tearDownAfterClass() throws Exception {
-}
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
 
-/**
- * @throws java.lang.Exception
- */
-@BeforeEach
-void setUp() throws Exception {
-}
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
 
-/**
- * @throws java.lang.Exception
- */
-@AfterEach
-void tearDown() throws Exception {
-}
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
 
-/**
- * Test method for {@link ExcelMethod#ExcelMethod(org.apache.poi.xssf.usermodel.XSSFRow)}.
- * @throws IOException 
- * @throws FileNotFoundException 
- */
-@Test
-final void testExcelMethod() throws FileNotFoundException, IOException {
-}
+	/**
+	 * Test method for
+	 * {@link ExcelMethod#ExcelMethod(org.apache.poi.xssf.usermodel.XSSFRow)}.
+	 * 
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 */
+	@Test
+	final void testExcelMethod() throws FileNotFoundException, IOException {
+	}
 
-/**
- * Test method for {@link ExcelMethod#getMethodID()}.
- */
-@Test
-final void testGetMethodID() {
-	assertEquals(methodID, excelMethod.getMethodID());
-}
+	/**
+	 * Test method for {@link ExcelMethod#getMethodID()}.
+	 */
+	@Test
+	final void testGetMethodID() {
+		assertEquals(methodID, excelMethod.getMethodID());
+	}
 
-/**
- * Test method for {@link ExcelMethod#getPackageName()}.
- */
-@Test
-final void testGetPackageName() {
-	assertEquals(packageName, excelMethod.getPackageName());
-}
+	/**
+	 * Test method for {@link ExcelMethod#getPackageName()}.
+	 */
+	@Test
+	final void testGetPackageName() {
+		assertEquals(packageName, excelMethod.getPackageName());
+	}
 
-/**
- * Test method for {@link ExcelMethod#getClassName()}.
- */
-@Test
-final void testGetClassName() {
-	assertEquals(className, excelMethod.getClassName());
-}
+	/**
+	 * Test method for {@link ExcelMethod#getClassName()}.
+	 */
+	@Test
+	final void testGetClassName() {
+		assertEquals(className, excelMethod.getClassName());
+	}
 
-/**
- * Test method for {@link ExcelMethod#getMethodName()}.
- */
-@Test
-final void testGetMethodName() {
-	assertEquals(methodName, excelMethod.getMethodName());
-}
+	/**
+	 * Test method for {@link ExcelMethod#getMethodName()}.
+	 */
+	@Test
+	final void testGetMethodName() {
+		assertEquals(methodName, excelMethod.getMethodName());
+	}
 
-/**
- * Test method for {@link ExcelMethod#getLoc()}.
- */
-@Test
-final void testGetLoc() {
-	assertEquals(loc, excelMethod.getLoc());
-}
+	/**
+	 * Test method for {@link ExcelMethod#getLoc()}.
+	 */
+	@Test
+	final void testGetLoc() {
+		assertEquals(loc, excelMethod.getLoc());
+	}
 
-/**
- * Test method for {@link ExcelMethod#getCyclo()}.
- */
-@Test
-final void testGetCyclo() {
-	assertEquals(cyclo, excelMethod.getCyclo());
-}
+	/**
+	 * Test method for {@link ExcelMethod#getCyclo()}.
+	 */
+	@Test
+	final void testGetCyclo() {
+		assertEquals(cyclo, excelMethod.getCyclo());
+	}
 
-/**
- * Test method for {@link ExcelMethod#getAtfd()}.
- */
-@Test
-final void testGetAtfd() {
-	assertEquals(atfd, excelMethod.getAtfd());
-}
+	/**
+	 * Test method for {@link ExcelMethod#getAtfd()}.
+	 */
+	@Test
+	final void testGetAtfd() {
+		assertEquals(atfd, excelMethod.getAtfd());
+	}
 
-/**
- * Test method for {@link ExcelMethod#getLaa()}.
- */
-@Test
-final void testGetLaa() {
-	assertEquals(laa, excelMethod.getLaa(), 0.0f);
-	//but why tho
-}
+	/**
+	 * Test method for {@link ExcelMethod#getLaa()}.
+	 */
+	@Test
+	final void testGetLaa() {
+		assertEquals(laa, excelMethod.getLaa(), 0.0f);
+		// but why tho
+	}
 
-/**
- * Test method for {@link ExcelMethod#isLongMethod()}.
- */
-@Test
-final void testIsLongMethod() {
-	assertEquals(isLongMethod, excelMethod.isLongMethod());
-}
+	/**
+	 * Test method for {@link ExcelMethod#isLongMethod()}.
+	 */
+	@Test
+	final void testIsLongMethod() {
+		assertEquals(isLongMethod, excelMethod.isLongMethod());
+	}
 
-/**
- * Test method for {@link ExcelMethod#getiPlasma()}.
- */
-@Test
-final void testGetiPlasma() {
-	assertEquals(iPlasma, excelMethod.getiPlasma());
-}
+	/**
+	 * Test method for {@link ExcelMethod#getiPlasma()}.
+	 */
+	@Test
+	final void testGetiPlasma() {
+		assertEquals(iPlasma, excelMethod.getiPlasma());
+	}
 
-/**
- * Test method for {@link ExcelMethod#getPmd()}.
- */
-@Test
-final void testGetPmd() {
-	assertEquals(pmd, excelMethod.getPmd());
-}
+	/**
+	 * Test method for {@link ExcelMethod#getPmd()}.
+	 */
+	@Test
+	final void testGetPmd() {
+		assertEquals(pmd, excelMethod.getPmd());
+	}
 
-/**
- * Test method for {@link ExcelMethod#isFeatureEnvy()}.
- */
-@Test
-final void testIsFeatureEnvy() {
-	assertEquals(isFeatureEnvy, excelMethod.isFeatureEnvy());
-}
+	/**
+	 * Test method for {@link ExcelMethod#isFeatureEnvy()}.
+	 */
+	@Test
+	final void testIsFeatureEnvy() {
+		assertEquals(isFeatureEnvy, excelMethod.isFeatureEnvy());
+	}
 
-/**
- * Test method for {@link ExcelMethod#toString()}.
- */
-@Test
-final void testToString() {
-	String toString = excelMethod.toString();
-	assertTrue(toString.contains("[ \n" + "MethodID: " + methodID +
-			"\n" + "Package: " + packageName + 
-			"\n" + "Class: " + className +
-			"\n" + "MethodName: " + methodName +
-			"\n" + "LOC: " + loc +
-			"\n" + "CYCLO: " + cyclo +
-			"\n" + "LAA: " + laa +
-			"\n" + "is_long_method:" + isLongMethod +
-			"\n" + "iPlasma: " + iPlasma +
-			"\n" + "PMD: " + pmd +
-			"\n" + "is_feature_envy: " + isFeatureEnvy +
-			"\n" + "]"));
-}
+	/**
+	 * Test method for {@link ExcelMethod#toString()}.
+	 */
+	@Test
+	final void testToString() {
+		String toString = excelMethod.toString();
+		assertTrue(toString.contains("[ \n" + "MethodID: " + methodID + "\n" + "Package: " + packageName + "\n"
+				+ "Class: " + className + "\n" + "MethodName: " + methodName + "\n" + "LOC: " + loc + "\n" + "CYCLO: "
+				+ cyclo + "\n" + "LAA: " + laa + "\n" + "is_long_method:" + isLongMethod + "\n" + "iPlasma: " + iPlasma
+				+ "\n" + "PMD: " + pmd + "\n" + "is_feature_envy: " + isFeatureEnvy + "\n" + "]"));
+	}
 
 }
